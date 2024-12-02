26e655f482a75f905136c80c9d52505f14a88698
joelepore <giuseppejoelepore@gmail.com>
2024-12-02 14:22:31 +0100

    feat: complete snack 7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,5 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+const student = students.filter(student => student.id === 2)[0];
+console.log(student);
